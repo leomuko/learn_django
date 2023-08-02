8f07f6ff71805baaa031bb551385a66fdf6f4130
leomuko <kezzieleo@gmail.com>
2023-08-02 20:22:58 +0300

    Write coverage tests for the new views

--- blog/blog/tests.py
+++ blog/blog/tests.py
@@ -18,6 +18,9 @@ class BlogTests(TestCase):
     def test_string_representation(self):
         post = Post(title='A sample title')
         self.assertEqual(str(post), post.title)
+    
+    def test_get_absolute_url(self):
+        self.assertEqual(self.post.get_absolute_url(), '/post/1/')
 
     def test_post_content(self):
         self.assertEqual(f'{self.post.title}', 'A good title')
@@ -31,10 +34,31 @@ class BlogTests(TestCase):
         self.assertTemplateUsed(response, 'home.html')
 
     def test_post_detail_view(self):
-        # response = self.client.get('/post/1/')
-        response = self.client.get(reverse('post_detail', args=[('1')]))
+        response = self.client.get('/post/1/')
+        # response = self.client.get(reverse('post_detail', args=[('1')]))
         no_response = self.client.get('/post/100000/')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, 'A good title')
         self.assertTemplateUsed(response, 'post_detail.html')
+
+    def test_post_create_view(self):
+        response = self.client.post(reverse('post_new'),{
+            'title': 'New Title',
+            'body' :'New Body',
+            'author': self.user.id
+        })
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Post.objects.last().title, 'New Title')
+        self.assertEqual(Post.objects.last().body, 'New Body')
+
+    def test_post_update_view(self):
+        response = self.client.post(reverse('post_edit', args=['1']), {
+            'title' : 'Updated Title',
+            'body' : 'Updated Body'
+        })
+        self.assertEqual(response.status_code, 302)
+
+    def test_post_delete_view(self):
+        response = self.client.post(reverse('post_delete', args='1'))
+        self.assertEqual(response.status_code, 302)
